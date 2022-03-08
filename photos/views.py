5ef6284aaec76f1ebfb3402603201981c61ff8aa
mohraf <amosngok@gmail.com>
2022-03-08 15:10:22 +0300

    create search view

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
   return render(request, 'index.html')
+
+
+def search_images_by_category(request, search_term):
+  images = Image.search_images(search_term)
+
+  context = {
+    "images": images
+  }
+
+  return render(request, 'search_results.html', context)
