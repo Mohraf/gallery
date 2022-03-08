c8616bd6c827c6593d7d5b912216c5b9888a270b
mohraf <amosngok@gmail.com>
2022-03-08 15:00:54 +0300

    add location model methods

--- photos/models.py
+++ photos/models.py
@@ -82,3 +82,19 @@ class Category(models.Model):
 
 class Location(models.Model):
   name = models.CharField(max_length=20)
+
+
+  def __str__(self):
+    return self.name
+  
+
+  def save_location(self):
+    self.save()
+
+  
+  def delete_location(self):
+    self.delete()
+  
+
+  def update_location(self):
+    self.update()
