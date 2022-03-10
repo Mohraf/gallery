1d0d31856b65b2f5f6e6143158dc820817c925a0
mohraf <amosngok@gmail.com>
2022-03-10 17:30:37 +0300

    conf django for heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -138,3 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
